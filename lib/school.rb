711739930a18bb3a68b6b6b50ae7c6cdc2a9ea6f
Bryanjazo <bryanjazo88@gmail.com>
2021-02-10 21:56:30 -0800

    commit tree 2c67fe7ffdd6c35fab4d7294f5f0cf99802df1b7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require "pry"
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, level)
+   roster[level] ||= []
+   roster[level] << student
+
+end
+
+def grade(level)
+    roster.detect do |x, y|
+      if x == level
+        #binding.pry
+        return y
+
+      end
+      #binding.pry
+      end
+    end
+
+    def sort
+      new_hash = {}
+      roster.each do |name,number|
+        new_hash[name] = number.sort
+      end
+      new_hash
+    end
+  end
